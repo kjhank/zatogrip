a6ad25ab9147c9284fc81ff54d794d358954759c
Krzysztof Hankiewicz <kjhank@TIE-Whisper.local>
2022-01-27 10:18:35 +0100

    hotfix/footnotes padding

--- src/components/HomePage/TilesSection/TilesSection.styled.js
+++ src/components/HomePage/TilesSection/TilesSection.styled.js
@@ -116,7 +116,7 @@ export const CloseButton = styled.button.attrs({ type: 'button' })`
 export const Footnotes = styled.footer`
   position: relative;
   z-index: 1;
-  padding-right: 30%;
+  padding-right: 40%;
   font-size: clamp(8px, 0.625vw, 12px);
   font-family: ${({ theme }) => theme.getFont('alt')};
 
