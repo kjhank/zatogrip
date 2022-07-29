004ec1a2fdda8476b302e733521a52813f974472
blazsak <blazsak@gmail.com>
2022-07-29 15:04:49 +0200

    add rel nofollow attribute for footer, product and post external links

--- src/components/Post/Footnotes.js
+++ src/components/Post/Footnotes.js
@@ -19,7 +19,7 @@ const renderFootnotes = content => {
           const isLink = footnote.startsWith('http');
           const item = isLink ?
             (
-              <ExternalLink href={footnote}>
+              <ExternalLink href={footnote} rel="noreferrer nofollow">
                 {footnote}
               </ExternalLink>
             ) :
