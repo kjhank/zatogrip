447f938f30c9736b7c1e6ebcc0927bd705665ba4
Robert-unbutu <r.smalarz@delante.pl>
2023-11-09 13:50:17 +0100

    e-melissa - remove attr nofollow

--- src/components/ProductPage/Header/Header.js
+++ src/components/ProductPage/Header/Header.js
@@ -87,7 +87,7 @@ export const Header = ({
               <ArrowLink
                 href={link.url}
                 key={link.title}
-                rel="noreferrer nofollow"
+                {...(link.title.includes('E-Melissa') ? {} : { rel: 'noreferrer nofollow' })}
                 target="_blank"
               >
                 {link.title}
